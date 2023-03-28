0525663d81bdaf374afbee2939203820fece5578
Nikos Karampas <nikos.karampas@hotmail.com>
2023-03-28 20:38:01 +0300

    Azure Key Vault integration, AddAzureKeyVault, CosmosDBOptions

--- IntegrateAzureKeyVault.cs
+++ IntegrateAzureKeyVault.cs
@@ -2,22 +2,28 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Company.Function
 {
     public class IntegrateAzureKeyVault
     {
         private readonly ILogger _logger;
+        private readonly CosmosDBOptions _cosmosDbOptions;
 
-        public IntegrateAzureKeyVault(ILoggerFactory loggerFactory)
+        public IntegrateAzureKeyVault(ILoggerFactory loggerFactory, IOptions<CosmosDBOptions> cosmosDbOptions)
         {
             _logger = loggerFactory.CreateLogger<IntegrateAzureKeyVault>();
+            this._cosmosDbOptions = cosmosDbOptions.Value;
         }
 
         [Function("IntegrateAzureKeyVault")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var cosmosDBConnectionString = _cosmosDbOptions.ConnectionString;
+            var cosmosDBAccessKey = _cosmosDbOptions.AccessKey;
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Company.Function;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices((hostingContext, services) => {
+        services.Configure<CosmosDBOptions>(hostingContext.Configuration.GetSection(nameof(CosmosDBOptions)));
+    })
+    .ConfigureAppConfiguration((hostingContext, configBuilder) => 
+    {
+        if (hostingContext.HostingEnvironment.IsProduction())
+        {
+            string? keyVaultEndpoint = Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT");
+
+            if (keyVaultEndpoint is null)
+                throw new InvalidOperationException("Store the Key Vault endpoint in a KEYVAULT_ENDPOINT environment variable.");
+
+            var secretClient = new SecretClient(new(keyVaultEndpoint), new DefaultAzureCredential());
+            configBuilder.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+        }
+    })
     .Build();
 
 host.Run();
